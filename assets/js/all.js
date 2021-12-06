9cd68aabe8197e8001f9b1ed350e2be9bb6f9e4d
calon <calonhu@protonmail.com>
2021-12-06 23:36:09 +0800

    Update 2021-12-06T15:36:08.310Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -13,12 +13,13 @@ var tokenObj = {
 }; // DOM
 
 var orderList = document.querySelector('[data-js="orderList"]');
-var orderTable = document.querySelector('.orderTable'); // data
+var orderTable = document.querySelector('.orderTable');
+var popUpDiv = document.querySelector('[data-js="popUpDiv"]'); // data
 
 var orderData; // event
 
-orderTable.addEventListener('click', deleteOrder);
-orderTable.addEventListener('click', isPaid);
+orderTable.addEventListener('click', doubleCheckMsg); // orderTable.addEventListener('click', isPaid);
+
 init();
 
 function init() {
@@ -31,7 +32,6 @@ function getOrderData() {
   axios.get("".concat(baseUrl, "/").concat(adminOrder_path), tokenObj).then(function (res) {
     orderData = res.data.orders;
     renderOrderList();
-    renderChart();
   })["catch"](function (err) {
     var errData = err.response.data;
 
@@ -46,26 +46,34 @@ function getOrderData() {
 ;
 
 function renderOrderList() {
-  var str = '';
-  orderData.forEach(function (item) {
-    var listStr = '';
-    item.products.forEach(function (item) {
-      listStr += "<li>".concat(item.title, "</li>");
+  var msg = document.querySelector('p[data-hasOrder]');
+
+  if (orderData.length === 0) {
+    msg.setAttribute('data-hasOrder', true);
+    orderTable.setAttribute('data-hasOrder', false);
+  } else {
+    msg.setAttribute('data-hasOrder', false);
+    orderTable.setAttribute('data-hasOrder', true);
+    var str = '';
+    orderData.forEach(function (item) {
+      var listStr = '';
+      item.products.forEach(function (item) {
+        listStr += "<li>".concat(item.title, "</li>");
+      });
+      var date = new Date(item.createdAt * 1000).toLocaleDateString();
+      str += "\n        <tr class=\"border-b\">\n          <td class=\"p-1.5 border-r\">".concat(item.id, "</td>\n          <td class=\"p-1.5 border-r\">\n            <ul>\n              <li>").concat(item.user.name, "</li>\n              <li>").concat(item.user.tel, "</li>\n            </ul>\n          </td>\n          <td class=\"p-1.5 border-r\">").concat(item.user.address, "</td>\n          <td class=\"p-1.5 border-r\">").concat(item.user.email, "</td>\n          <td class=\"p-1.5 border-r\">\n            <ul>").concat(listStr, "</ul>\n          </td>\n          <td class=\"p-1.5 border-r text-center\">").concat(date, "</td>\n          <td class=\"p-1.5 border-r text-center\">\n            <a class=\"text-info underline\" href=\"#\" data-id=\"").concat(item.id, "\" data-js=\"paidBtn\">").concat(item.paid ? '已處理' : '未處理', "</a>\n          </td>\n          <td class=\"p-1.5 text-center\">\n            <button class=\"text-white bg-danger hover:opacity-70 rounded py-1.5 px-2.5\" data-js=\"deleteOrderBtn\" data-id=\"").concat(item.id, "\">\u522A\u9664</button>\n          </td>\n        </tr>\n    ");
     });
-    var date = new Date(item.createdAt * 1000).toLocaleDateString();
-    str += "\n      <tr class=\"border-b\">\n        <td class=\"p-1.5 border-r\">".concat(item.id, "</td>\n        <td class=\"p-1.5 border-r\">\n          <ul>\n            <li>").concat(item.user.name, "</li>\n            <li>").concat(item.user.tel, "</li>\n          </ul>\n        </td>\n        <td class=\"p-1.5 border-r\">").concat(item.user.address, "</td>\n        <td class=\"p-1.5 border-r\">").concat(item.user.email, "</td>\n        <td class=\"p-1.5 border-r\">\n          <ul>").concat(listStr, "</ul>\n        </td>\n        <td class=\"p-1.5 border-r text-center\">").concat(date, "</td>\n        <td class=\"p-1.5 border-r text-center\">\n          <a class=\"text-info underline\" href=\"#\" data-id=\"").concat(item.id, "\" data-js=\"paidBtn\">").concat(item.paid ? '已處理' : '未處理', "</a>\n        </td>\n        <td class=\"p-1.5 text-center\">\n          <button class=\"text-white bg-danger hover:opacity-70 rounded py-1.5 px-2.5\" data-js=\"deleteOrderBtn\" data-id=\"").concat(item.id, "\">\u522A\u9664</button>\n        </td>\n      </tr>\n  ");
-  });
-  orderList.innerHTML = str;
+    orderList.innerHTML = str;
+    renderChart();
+  }
+
+  ;
 }
 
 ;
 
-function deleteOrder(e) {
-  var targetJs = e.target.dataset.js;
-
-  if (targetJs !== 'deleteOrderBtn' && targetJs !== 'deleteAllOrderBtn') {
-    return;
-  } else if (targetJs === 'deleteAllOrderBtn') {
+function deleteOrder(id, btnProp) {
+  if (btnProp === 'deleteAll') {
     axios["delete"]("".concat(baseUrl, "/").concat(adminOrder_path), tokenObj).then(function (res) {
       getOrderData();
     })["catch"](function (err) {
@@ -76,9 +84,10 @@ function deleteOrder(e) {
       }
 
       ;
+    }).then(function () {
+      popUpDiv.setAttribute('data-popUp', false);
     });
-  } else if (targetJs === 'deleteOrderBtn') {
-    var id = e.target.dataset.id;
+  } else if (btnProp === 'deleteOne') {
     axios["delete"]("".concat(baseUrl, "/").concat(adminOrder_path, "/").concat(id), tokenObj).then(function (res) {
       getOrderData();
     })["catch"](function (err) {
@@ -89,6 +98,8 @@ function deleteOrder(e) {
       }
 
       ;
+    }).then(function () {
+      popUpDiv.setAttribute('data-popUp', false);
     });
   }
 
@@ -97,22 +108,14 @@ function deleteOrder(e) {
 
 ;
 
-function isPaid(e) {
-  e.preventDefault();
-
-  if (e.target.dataset.js !== 'paidBtn') {
-    return;
-  }
-
-  var id = e.target.dataset.id;
+function isPaid(id, status) {
   var obj = {
     "data": {
       "id": id,
-      "paid": true
+      "paid": !status
     }
   };
   axios.put("".concat(baseUrl, "/").concat(adminOrder_path), obj, tokenObj).then(function (res) {
-    console.log(res);
     getOrderData();
   })["catch"](function (err) {
     var errData = err.response.data;
@@ -122,6 +125,8 @@ function isPaid(e) {
     }
 
     ;
+  }).then(function () {
+    popUpDiv.setAttribute('data-popUp', false);
   });
 }
 
@@ -201,4 +206,65 @@ function renderChart() {
     }
   });
 }
+
+;
+
+function doubleCheckMsg(e) {
+  e.preventDefault();
+  var targetJs = e.target.dataset.js;
+
+  if (targetJs !== 'deleteOrderBtn' && targetJs !== 'deleteAllOrderBtn' && targetJs !== 'paidBtn') {
+    return;
+  }
+
+  ;
+  var popUpMsg = document.querySelector('[data-js="popUpMsg"]');
+  var orderId;
+  var target;
+  var str = '';
+  popUpDiv.setAttribute('data-popUp', true);
+
+  if (targetJs == 'deleteAllOrderBtn') {
+    str += "\n      <div class=\"popUp-body pr-16 mb-2\">\n      <p class=\"text-lg pb-1.5\">\u78BA\u5B9A\u8981\u522A\u9664<strong>\u5168\u90E8\u8A02\u55AE</strong>\u55CE\uFF1F</p>\n      </div>\n      <div class=\"popUp-footer flex justify-end\">\n      <button class=\"mr-2 py-2 px-3 bg-gray-400 text-white rounded hover:opacity-75\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"false\">\u53D6\u6D88</button>\n      <button class=\"py-2 px-3 bg-danger text-white rounded hover:opacity-75\" data-prop=\"deleteAll\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"true\">\u522A\u9664</button>\n      </div>\n      ";
+  } else {
+    orderId = e.target.dataset.id;
+    target = orderData.filter(function (item) {
+      return item.id == orderId;
+    });
+    target = target[0];
+    var productStr = '';
+    target.products.forEach(function (item) {
+      productStr += "<li>".concat(item.title, "</li>");
+    });
+
+    if (targetJs == 'deleteOrderBtn') {
+      str += "\n        <div class=\"popUp-body p-4 border-b\">\n          <p class=\"text-lg pb-1.5\">\u78BA\u5B9A\u8981\u522A\u9664\u4EE5\u4E0B\u8A02\u55AE\u55CE\uFF1F</p>\n          <hr>\n          <ul class=\"mt-4\"}>\n            <li>\u8A02\u55AE\u7DE8\u865F\uFF1A".concat(target.id, "</li>\n            <li>\u9867\u5BA2\u59D3\u540D\uFF1A").concat(target.user.name, "</li>\n            <li>\n              \u8CFC\u8CB7\u7522\u54C1\uFF1A\n              <ul class=\"list-disc ml-8\">").concat(productStr, "</ul>\n            </li>\n            <li>\u7E3D\u91D1\u984D\uFF1A").concat(target.total, "\n        </div>\n        <div class=\"popUp-footer flex justify-end py-4\">\n          <button class=\"mr-2 py-2 px-3 bg-gray-400 text-white rounded hover:opacity-75\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"false\">\u53D6\u6D88</button>\n          <button class=\"py-2 px-3 bg-danger text-white rounded hover:opacity-75\" data-prop=\"deleteOne\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"true\">\u522A\u9664</button>\n        </div>\n      ");
+    } else if (targetJs === 'paidBtn') {
+      str += "\n        <div class=\"popUp-body p-4 border-b\">\n          <p class=\"text-lg pb-1.5\">\n            \u78BA\u5B9A\u8981\u5C07\u4EE5\u4E0B\u8A02\u55AE\u7684\u72C0\u614B\u5F9E <span class=\"text-info underline\">".concat(target.paid ? '已處理' : '未處理', "</span> \u6539\u6210 \n            <span class=\"text-info underline\">").concat(target.paid ? '未處理' : '已處理', "</span> \u55CE\uFF1F\n          </p>\n          <hr>\n          <ul class=\"mt-4\"}>\n            <li>\u8A02\u55AE\u7DE8\u865F\uFF1A").concat(target.id, "</li>\n            <li>\u9867\u5BA2\u59D3\u540D\uFF1A").concat(target.user.name, "</li>\n            <li>\n              \u8CFC\u8CB7\u7522\u54C1\uFF1A\n              <ul class=\"list-disc ml-8\">").concat(productStr, "</ul>\n            </li>\n            <li>\u7E3D\u91D1\u984D\uFF1A").concat(target.total, "\n        </div>\n        <div class=\"popUp-footer flex justify-end py-4\">\n          <button class=\"mr-2 py-2 px-3 bg-gray-400 text-white rounded hover:opacity-75\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"false\">\u53D6\u6D88</button>\n          <button class=\"py-2 px-3 bg-danger text-white rounded hover:opacity-75\" data-prop=\"changePaid\" data-js=\"dblCheckBtn\" data-dblCheckBtn=\"true\">\u78BA\u5B9A</button>\n        </div>\n      ");
+    }
+
+    ;
+  }
+
+  ;
+  popUpMsg.innerHTML = str;
+  popUpDiv.addEventListener('click', function (e) {
+    var targetJs = e.target.dataset.js;
+    var btnProp = e.target.dataset.prop;
+
+    if (targetJs !== 'popUpDiv' && targetJs !== 'dblCheckBtn') {
+      return;
+    } else if (Object.is(popUpDiv, e.target) || e.target.dataset.dblcheckbtn === 'false') {
+      popUpDiv.setAttribute('data-popUp', false);
+    } else if (btnProp === 'changePaid') {
+      isPaid(orderId, target.paid);
+    } else if (btnProp === 'deleteOne' || btnProp === 'deleteAll') {
+      deleteOrder(orderId, btnProp);
+    }
+
+    ;
+  });
+}
+
+;
 //# sourceMappingURL=all.js.map

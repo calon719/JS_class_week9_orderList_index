52b4d5fefe291fbca65d8ca956942f5ce6c169b2
calon <calonhu@protonmail.com>
2021-12-09 15:46:45 +0800

    壓縮 css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: {
-    enabled: false,
+    enabled: true,
     content: ["./app/**/*.html", "./app/assets/style/**/*.scss", "./app/**/*.js", "./app/**/*.ejs"],
   },
   darkMode: false, // or 'media' or 'class'
